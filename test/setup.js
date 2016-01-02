074a9b2dd1e112ffd20d9dbf85f56a1b369d26b4
Andre Mekkawi <github@andremekkawi.com>
2016-01-02 16:14:37 -0500

    Add test helper for context

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,8 @@
 import { jsdom } from 'jsdom'
+import expect from 'expect';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
 
 global.document = jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
@@ -21,4 +23,36 @@ export function getAttrs(el, attrs) {
 
 export function getCAttrs(component, attrs) {
 	return getAttrs(ReactDOM.findDOMNode(component), attrs);
+}
+
+export function expectContext(ContextComponent, Component, childContextTypes, contextProps) {
+	const OuterContext = React.createClass({
+		childContextTypes,
+		getChildContext() {
+			return contextProps;
+		},
+		render() {
+			return this.props.children;
+		}
+	});
+
+	const tree = TestUtils.renderIntoDocument(
+		<OuterContext>
+			<ContextComponent/>
+		</OuterContext>
+	);
+
+	const contextKeys = Object.keys(ContextComponent.contextTypes).sort();
+	expect(typeof ContextComponent.contextTypes).toBe('object', 'context is object');
+	expect(contextKeys).toEqual(Object.keys(contextProps).sort(), 'context keys match');
+
+	const contextChild = TestUtils.findRenderedComponentWithType(tree, ContextComponent);
+	for (let i = 0; i < contextKeys.length; i++) {
+		expect(contextChild.context[contextKeys[i]]).toBe(contextProps[contextKeys[i]], `context prop "${contextKeys[i]}" received from outer`);
+	}
+
+	const circlesChild = TestUtils.findRenderedComponentWithType(tree, Component);
+	for (let i = 0; i < contextKeys.length; i++) {
+		expect(circlesChild.props[contextKeys[i]]).toBe(contextProps[contextKeys[i]], `context prop "${contextKeys[i]}" passed as prop`);
+	}
 }
